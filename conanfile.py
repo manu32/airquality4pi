024e5b33d2f02e84074810bb6fbfef45b0643b90
manu32 <manuelaigner@gmx.de>
2021-10-05 20:59:33 +0200

    add initial project files for build environment

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+
+import platform
+from conans import ConanFile, CMake, tools
+
+
+class ConanRecipe(ConanFile):
+
+    name = "airquality4pi"
+    version = "1.0.0-testing"
+    description = ""
+    author = "manu32"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    
+    def build_requirements(self):
+        self.build_requires("gtest/1.8.1@bincrafters/stable")
+        
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout<< "setup done" << std::endl;
+
+    return 0;
+}
