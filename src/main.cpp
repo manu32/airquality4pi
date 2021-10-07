41050750da9e3b2f6d37625f2e966fdbb7850b02
manu32 <manuelaigner@gmx.de>
2021-10-07 21:30:42 +0200

    add simple test of dht sensor to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 
+#include "dht_sensor/dht_sensor.h"
+
 #include <iostream>
+#include <thread>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    std::cout<< "setup done" << std::endl;
+    std::cout << "setup done" << std::endl;
 
+    rpi::dht::DHTSensor dht;
+
+    for (auto count = 0; count < 100U; ++count)
+    {
+        auto result = dht.measure();
+
+        if (result.has_value())
+        {
+            std::cout << "temp: " << result.value().temperature()
+                      << "°C ,humidity: " << result.value().humidity() << " %"
+                      << std::endl;
+            break;
+        }
+        std::cout << count << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    std::cout << "finished dht sensor example" << std::endl;
     return 0;
 }
