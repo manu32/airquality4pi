c76f80fc3776900beda35d398e3d1c10feabd834
manu32 <manuelaigner@gmx.de>
2021-10-08 21:59:19 +0200

    forward architecture setting to cmake and add sqlite3 to dependencies

--- conanfile.py
+++ conanfile.py
@@ -14,9 +14,11 @@ class ConanRecipe(ConanFile):
     
     def build_requirements(self):
         self.build_requires("gtest/1.8.1@bincrafters/stable")
+        self.build_requires("sqlite3/3.36.0")
         
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["ARCH"] = self.settings.arch
         cmake.configure()
         cmake.build()
         cmake.test()
