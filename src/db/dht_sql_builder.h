b37cbe857c684aa6fc9cd7354519df0ac5042193
manu32 <manuelaigner@gmx.de>
2021-10-13 20:38:14 +0200

    derive class for sql insertion of dht readings

--- src/db/dht_sql_builder.h
+++ src/db/dht_sql_builder.h
@@ -2,67 +2,44 @@
 #ifndef DHT_SQL_BUILDER_
 #define DHT_SQL_BUILDER_
 
-#include <string>
-#include <sstream>
-#include <chrono>
-#include <iomanip>
+#include "sql_builder.h"
 
-class DHTSQLBuilder
+class DHTReadingSQLBuilder : public SQLInsertBuilder
 {
 
-::std::stringstream m_stream;
-
-public:
-
-    DHTSQLBuilder()
-    : m_stream()
+  public:
+    DHTReadingSQLBuilder()
     {
-        m_stream << "INSERT INTO ";
     }
 
-    DHTSQLBuilder& insert_measurement_into_table(std::string table)
-    {  
-        m_stream << table << " (HUMIDITY,TEMPERATURE,CURRENTDATE,CURRENTIME) VALUES (";
+    DHTReadingSQLBuilder &insert_temperature(const float &temperature)
+    {
+        insert("temperature", temperature);
         return *this;
     }
 
-    DHTSQLBuilder& with_humidity(float humidity)
+    DHTReadingSQLBuilder &insert_humidity(const float &humidity)
     {
-        m_stream << std::fixed << std::setprecision(2) << humidity << ",";
+        insert("humidity", humidity);
         return *this;
     }
 
-    DHTSQLBuilder& with_temperature(float temperature)
+    DHTReadingSQLBuilder &now()
     {
-        m_stream << std::fixed << std::setprecision(2) << temperature << ",";
+        auto in_time_t = std::chrono::system_clock::to_time_t(
+            std::chrono::system_clock::now());
+        insert("currentdate", time_t_to_str(in_time_t, time_format::date()));
+        insert("currentime", time_t_to_str(in_time_t, time_format::time()));
         return *this;
     }
 
-    DHTSQLBuilder& on_date(std::string date)
+    DHTReadingSQLBuilder &at_time(const std::chrono::system_clock::time_point& time)
     {
-        m_stream << date << ",";
+        auto in_time_t = std::chrono::system_clock::to_time_t(time);
+        insert("currentdate", time_t_to_str(in_time_t, time_format::date()));
+        insert("currentime", time_t_to_str(in_time_t, time_format::time()));
         return *this;
     }
-
-    DHTSQLBuilder& at_time(std::string time)
-    {
-        m_stream << time << ");";
-        return *this;
-    }
-
-    DHTSQLBuilder& with_current_date_and_time()
-    {
-        auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        m_stream << std::put_time(std::localtime(&time), "%Y-%m-%d") << "," << 
-        std::put_time(std::localtime(&time), "%H:%M:%S") << ");";
-        return *this;
-    }
-
-    ::std::string command() 
-    {
-        return m_stream.str();
-    }
-
 };
 
 #endif // DHT_SQL_BUILDER_
